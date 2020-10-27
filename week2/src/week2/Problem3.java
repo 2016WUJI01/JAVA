b0d2dcc1ec409ae37b851c4c7458ef6df2c56f0a
2016WUJI01 <49804204+2016WUJI01@users.noreply.github.com>
2020-10-27 13:50:34 +0800

    Delete Problem3.java

--- week2/src/week2/Problem3.java
+++
@@ -1,9 +0,0 @@
-package week2;
-
-public class Problem3 {
-	public static void main(String[] args) {
-		for(int i=0; i<args.length; i++) {
-			System.out.println("第"+(i+1)+"个字符串个数为"+args[i].length());
-		}
-	}
-}
