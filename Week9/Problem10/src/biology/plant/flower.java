099295105e8723696ccdab9773bff247aa6617b7
2016WUJI01 <49804204+2016WUJI01@users.noreply.github.com>
2020-11-17 11:45:01 +0800

    Add files via upload

---
+++ Week9/Problem10/src/biology/plant/flower.java
@@ -0,0 +1,34 @@
+package biology.plant;
+
+public class flower {
+	private String name;
+	private String height;
+	private String weight;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public void eat() {
+		
+	}
+	public void sleep() {
+		
+	}
+	public void work() {
+		
+	}
+}
---
+++ Week9/Problem10/src/school/garden/TestDemo.java
@@ -0,0 +1,10 @@
+package school.garden;
+import biology.animal.human;
+import biology.plant.flower;
+
+
+public class TestDemo {
+	public static void main(String[] args) {
+		
+	}
+}
